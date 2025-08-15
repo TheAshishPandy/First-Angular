f2ac763526beaae4b2e1395be40b28793f1a74c9
Ashish Pandey <Ashish.pandey@businessnext.com>
2025-08-15 10:38:40 +0530

    data

---
+++ src/app/profile/profile.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core'
+import { CommonModule } from '@angular/common';
+
+@Component({
+    selector:'profile',
+    template:`<h1> Profile </h1>`,
+    styles:`backgound:Orange,color:white`,
+
+})
+
+export class Profile{}
